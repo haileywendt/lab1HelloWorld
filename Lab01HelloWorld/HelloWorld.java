728ef7f17f15f1deb4d874e2835e23286d3f449c
Hailey Wendt <89602467+haileywendt@users.noreply.github.com>
2022-01-25 12:48:58 -0500

    Add files via upload

---
+++ Lab01HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * HelloWorld - 1st use of Java using the BlueJ IDE
+ *
+ * @author Hailey M. Wendt
+ * @version 1.0 - 25 Jan 2022
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
